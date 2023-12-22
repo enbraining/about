1666586acdabeb481001a4623225863cc670cf1d
enbraining <me@xolving.com>
2023-12-23 00:59:54 +0900

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
-import 'css/tailwind.css'
+import 'css/globals.css'
 import 'pliny/search/algolia.css'
 
 import Header from '@/components/Header'
 import siteMetadata from '@/data/siteMetadata'
 import { Metadata } from 'next'
+import { Nanum_Gothic } from 'next/font/google'
 import { SearchConfig, SearchProvider } from 'pliny/search'
 import { ThemeProviders } from './theme-providers'
+
+const nanum_gothic = Nanum_Gothic({
+  subsets: ['latin'],
+  weight: '400',
+})
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
@@ -49,12 +55,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang={siteMetadata.language} className="scroll-smooth" suppressHydrationWarning>
+    <html
+      lang={siteMetadata.language}
+      className={`${nanum_gothic} scroll-smooth`}
+      suppressHydrationWarning
+    >
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
-      <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
+      <body>
         <ThemeProviders>
-          <div className="flex h-screen flex-col justify-between font-sans">
+          <div className={`flex h-screen flex-col justify-between font-sans`}>
             <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
               <Header />
               <main className="mb-auto">{children}</main>
